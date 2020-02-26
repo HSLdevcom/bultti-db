1cca807014605690822912277549df619d6fee4e
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-02-26 14:27:32 +0200

    Generated DDL for database, script improvements.

--- export/process_lines.sh
+++ export/process_lines.sh
@@ -5,5 +5,11 @@
 
 for table in $(<tables.lst); do
   echo "$table"
-  sed -r -e "s/,//g" ${table}.csv | sed -r -e "s/@|@/,/g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" > ../processed/${table}.csv
+  # Remove all commas,
+  # convert the delimiter (@|@) to a comma,
+  # make all NULL strings empty values,
+  # remove second row (--- separator),
+  # remove last row (empty),
+  # then remove all empty rows.
+  sed -r -e "s/,//g" ${table}.csv | sed -r -e "s/@|@/,/g" | sed -e "s/NULL//g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" > ../processed/${table}.csv
 done
