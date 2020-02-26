09e1b21685905775619efc41e5b3b5dc04f392f6
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-02-26 14:27:32 +0200

    Generated DDL for database, script improvements.

--- export/export_tables.sh
+++ export/export_tables.sh
@@ -3,11 +3,18 @@
 # Make sure a tables.lst file exists in the same location with a list of each table to download.
 # @file: bcp-dump.sh
 server="10.218.6.14,56239" # Change this.
-user="jorep" # Change this.
-pass="tuotanto" # Change this.
+user="jorep"               # Change this.
+pass="tuotanto"            # Change this.
 creds="-S '$server' -U '$user' -P '$pass'"
 
 for table in $(<tables.lst); do
-  sqlcmd -S 10.218.6.14,56239 -U jorep -P "tuotanto" -s"@|@" -W -Q "SET NOCOUNT ON; SELECT * FROM $table" > ${table}.tmp
-  sed -r -e "s/,//g" ${table}.tmp | sed -r -e "s/@|@/,/g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" > ${table}.csv
+  sqlcmd -S 10.218.6.14,56239 -U jorep -P "tuotanto" -s"@@@" -W -Q "SET NOCOUNT ON; SELECT * FROM ${table}" >${table}.tmp
+
+  # Remove all commas,
+  # convert the delimiter (@@@) to a comma,
+  # make all NULL strings empty values,
+  # remove second row (--- separator),
+  # remove last row (empty),
+  # then remove all empty rows.
+  sed -r -e "s/,//g" ${table}.tmp | sed -r -e "s/@@@/,/g" | sed -e "s/NULL//g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" >${table}.csv
 done
