625ac4bf01a5b957a3df2c759dfd3c5286a03233
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-03-02 06:59:19 +0200

    Attempt to fix export file output.

--- export/export_tables.sh
+++ export/export_tables.sh
@@ -8,7 +8,7 @@ pass="tuotanto"            # Change this.
 creds="-S '$server' -U '$user' -P '$pass'"
 
 for table in $(<tables.lst); do
-  sqlcmd "${creds}" -s"|" -W -Q "SET NOCOUNT ON; SELECT * FROM ${table}" > "${table}.tmp"
+  sqlcmd "${creds}" -s"@" -W -Q "SET NOCOUNT ON; SELECT * FROM ${table}" > "${table}.tmp"
 
   # Remove all commas,
   # convert the delimiter (|) to a comma,
@@ -16,5 +16,5 @@ for table in $(<tables.lst); do
   # remove second row (--- separator),
   # remove last row (empty),
   # then remove all empty rows.
-  sed -e "s/,//g" "${table}.tmp" | sed -e "s/\|/,/g" | sed -e "s/NULL//g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" > "${table}.csv"
+  sed -e "s/,//g" "${table}.tmp" | sed -e "s/@/,/g" | sed -e "s/NULL//g" | sed -e "2d" | sed -e "$d" | sed -e "/^$/d" > "${table}.csv"
 done
