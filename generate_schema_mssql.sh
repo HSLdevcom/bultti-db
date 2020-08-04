5ac21ca5528e1c54f7c693aaac7de7ab7a202de9
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-08-04 10:04:35 +0300

    Test mssql db WIP.

--- generate_schema_mssql.sh
+++ generate_schema_mssql.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-export PGPASSWORD=password
-psql -h localhost -p 5432 -U postgres -c "create schema if not exists jore; alter schema jore owner to postgres;"
-
 PWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Collect all csv files in the data dir
@@ -18,5 +15,5 @@ for f in $FILES; do
   # Create tables automatically with csvkit
   # Only read the first 10k lines, otherwise csvkit takes forever (10k is already slow).
   # The column types are inferred from the data, so we can't take too small of a sample.
-  head -n 10000 "${f}" | csvsql --db postgresql://postgres:password@localhost:5432/postgres --tables "${name}" --db-schema jore --no-constraints --maxfieldsize 10000000000
+  head -n 10000 "${f}" | csvsql --db 'mssql://sa:yourStrong(!)Password@MSSQLServerDatabase' --tables "${name}" --db-schema jore --no-constraints --maxfieldsize 10000000000 --datetime-format datetime
 done
