efd4bf8b7d3d2777d780282a88a9fac7a8adcc93
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-08-05 16:00:47 +0300

    Timing, filtering, mapping, sync seems to work.

--- app/sync.js
+++ app/sync.js
@@ -1,15 +1,25 @@
 import fs from 'fs-extra';
 import path from 'path';
-import { trim, mapValues, toString } from 'lodash';
+import { trim, mapValues, get, floor } from 'lodash';
 import * as mssql from 'mssql';
-import { MSSQL_CONNECTION, BATCH_SIZE } from '../constants';
+import { MSSQL_CONNECTION, BATCH_SIZE, NS_PER_SEC } from '../constants';
 import { getPrimaryConstraint } from './getPrimaryConstraint';
 import { upsert } from './upsert';
 import { getKnex } from './knex';
 import PQueue from 'p-queue';
-import { formatISO } from 'date-fns';
+import { parseISO, isAfter, isValid } from 'date-fns';
 
 const { knex } = getKnex();
+
+function echoTime(message = 'Database synced', time) {
+  if (!time) {
+    console.log(`${message}. (time unavailable)`);
+  } else {
+    let [execS, execNs] = process.hrtime(time);
+    let ms = (execS * NS_PER_SEC + execNs) / 1000000;
+    console.log(`${message} in ${floor(ms / 1000, 3)} s`);
+  }
+}
 
 async function getTables() {
   let tablesFile = await fs.readFile(path.join(__dirname, '..', 'tables.lst'), 'utf8');
@@ -17,12 +27,26 @@ async function getTables() {
   return tablesFile
     .split('\n')
     .map((tableName) => trim(tableName))
-    .filter((tableName) => !!tableName);
+    .filter((tableName) => !!tableName && !tableName.startsWith('#'));
 }
 
-function sanitizeRow(row) {
-  return mapValues(row, (key, val) => {
-    if (typeof val === 'string') {
+function createInputRow(row, columnSchema) {
+  return mapValues(row, (val, key) => {
+    let columnType = get(columnSchema, `${key}.type`);
+
+    if (columnType === 'boolean' && typeof val !== 'boolean') {
+      let baseVal = val;
+
+      if (typeof val === 'string') {
+        baseVal = trim(val);
+      } else if (typeof val === 'number') {
+        baseVal = baseVal > 0;
+      }
+
+      return !!baseVal;
+    }
+
+    if (columnType.includes('char')) {
       let trimmedVal = trim(val);
 
       if (!trimmedVal) {
@@ -32,11 +56,79 @@ function sanitizeRow(row) {
       return trimmedVal;
     }
 
-    if (val instanceof Date) {
-      return formatISO(val);
+    if (columnType === 'numeric' && typeof val === 'string') {
+      let parsed = val.includes('.') ? parseFloat(val) : parseInt(val, 10);
+
+      if (isNaN(parsed)) {
+        return null;
+      }
+
+      return parsed;
     }
 
     return val;
+  });
+}
+
+// Inlude only rows that are active after 2019
+let cutoffDate = parseISO('2019-01-01');
+let ignoreDateCols = [
+  'muutospvm',
+  'perustpvm',
+  'tallpvm',
+  'liialkpvm',
+  'liipaattpvm',
+  'kohtarjouspvm',
+  'kohindeksipvm',
+  'kohviimpvm',
+  'sloppupvm',
+  'liiviimpvm',
+];
+
+let ignoreCutoffForTables = [
+  'jr_ajoneuvo',
+  'ak_kalusto',
+  'jr_eritpvkalent',
+  'jr_kinf_kalusto',
+];
+
+// Filter function to only include rows that are active after the cutoff date.
+function dateCutoffFilter(row, columnSchema) {
+  let dateColumns = Object.entries(columnSchema).reduce((dateCols, [colName, schema]) => {
+    if (
+      !ignoreDateCols.includes(colName) &&
+      ['timestamp', 'date'].some((match) => schema.type.startsWith(match))
+    ) {
+      dateCols.push(colName);
+    }
+
+    return dateCols;
+  }, []);
+
+  if (dateColumns.length === 0) {
+    return true;
+  }
+
+  let dateValues = [];
+
+  for (let col of dateColumns) {
+    let dateVal = row[col];
+
+    if (dateVal) {
+      let dateObj = dateVal instanceof Date ? dateVal : parseISO(dateVal);
+
+      if (isValid(dateObj)) {
+        dateValues.push(dateObj);
+      }
+    }
+  }
+
+  if (dateValues.length === 0) {
+    return true;
+  }
+
+  return dateValues.some((dateVal) => {
+    return isAfter(dateVal, cutoffDate);
   });
 }
 
@@ -47,16 +139,25 @@ async function createInsertForTable(tableName) {
     .table(tableName)
     .columnInfo();
 
-  console.log(columnSchema);
-
   return (data) => {
-    let processedRows = data.map((row) => sanitizeRow(row));
+    let processedRows = data
+      .filter((row) => {
+        if (ignoreCutoffForTables.includes(tableName)) {
+          return true;
+        }
+
+        return dateCutoffFilter(row, columnSchema);
+      })
+      .map((row) => createInputRow(row, columnSchema));
+
     return upsert(tableName, processedRows, constraint);
   };
 }
 
 function syncTable(tableName) {
   return new Promise(async (resolve, reject) => {
+    let tableTime = process.hrtime();
+
     let request = new mssql.Request();
     request.stream = true;
     request.query(`SELECT * FROM dbo.${tableName}`);
@@ -86,7 +187,7 @@ function syncTable(tableName) {
 
     request.on('done', async () => {
       await processRows();
-      resolve();
+      resolve(tableTime);
     });
 
     request.on('error', reject);
@@ -94,11 +195,9 @@ function syncTable(tableName) {
 }
 
 export async function syncSourceToDestination() {
-  await mssql.connect({
-    ...MSSQL_CONNECTION,
-    stream: true,
-  });
+  let syncTime = process.hrtime();
 
+  await mssql.connect(MSSQL_CONNECTION);
   let tables = await getTables();
 
   let syncQueue = new PQueue({
@@ -106,19 +205,21 @@ export async function syncSourceToDestination() {
     autoStart: true,
   });
 
-  for (let tableName of tables) {
-    console.log(`Importing ${tableName} from source.`);
-
-    syncQueue
-      .add(() => syncTable(tableName))
-      .then(() => console.log(`${tableName} imported.`));
-  }
-
   try {
-    await syncQueue.onEmpty();
+    for (let tableName of tables) {
+      console.log(`Importing ${tableName} from source.`);
+
+      syncQueue
+        .add(() => syncTable(tableName))
+        .then((tableTime) => echoTime(`${tableName} imported`, tableTime));
+    }
+
+    syncQueue.start();
+
+    await syncQueue.onIdle();
   } catch (err) {
     console.log(err);
   }
 
-  console.log('Sync complete.');
+  echoTime('Sync complete', syncTime);
 }
