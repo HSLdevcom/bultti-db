b8425ac33c2f241928144f37b524049d2969e952
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-08-27 06:39:10 +0300

    Fix primary keys filter.

--- app/utils/notNullFilter.js
+++ app/utils/notNullFilter.js
@@ -22,10 +22,14 @@ export function createNotNullFilter(constraint, columnSchema) {
 
       // All numbers are valid, but 0 will evaluate to false. Set val to 1 to avoid this.
       if (typeof val === 'number') {
+        if(isNaN(val)) {
+          return false
+        }
+        
         val = 1;
       }
 
-      return !!val;
+      return val !== null && typeof val !== 'undefined' && val !== ''
     });
   };
 }
