afae5ee2f6a032a6d85129e231b494d1428f82f2
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-09-01 06:59:12 +0300

    Docker setup, set memory limit on production node process.

--- deploy-all.sh
+++ deploy-all.sh
@@ -5,9 +5,10 @@ set -e
 
 ORG=${ORG:-hsldevcom}
 
-for TAG in latest dev stage production; do
-  DOCKER_IMAGE=$ORG/bultti-db:${TAG}
+# dev stage production
+TAG="latest"
 
-  docker build -t $DOCKER_IMAGE .
-  docker push $DOCKER_IMAGE
-done
+DOCKER_IMAGE="${ORG}/bultti-db:${TAG}"
+
+docker build -t "$DOCKER_IMAGE" .
+docker push "$DOCKER_IMAGE"
