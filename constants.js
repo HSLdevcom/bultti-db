55a39b0c853fa9cee20ba440f3784b1c9be0db1b
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-09-15 13:22:30 +0300

    Add port to mssql connection.

--- constants.js
+++ constants.js
@@ -44,6 +44,7 @@ export const MSSQL_CONNECTION = {
   password: secretsEnv.MSSQLPASSWORD,
   server: secretsEnv.MSSQLHOST,
   database: secretsEnv.MSSQLDATABASE,
+  port: secretsEnv.MSSQLPORT,
 };
 
 export const DEBUG = secretsEnv.DEBUG || 'false';
@@ -56,5 +57,5 @@ export const ENVIRONMENT = secretsEnv.ENVIRONMENT || 'unknown';
 export const MONITORING_ENABLED = secretsEnv.MONITORING_ENABLED === 'true';
 export const BATCH_SIZE = 1000;
 export const NS_PER_SEC = 1e9; // For tracking performance
-export const WRITE_SCHEMA_NAME = '_jore_import'
-export const READ_SCHEMA_NAME = 'jore'
+export const WRITE_SCHEMA_NAME = '_jore_import';
+export const READ_SCHEMA_NAME = 'jore';
