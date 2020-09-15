128183feb7c1770d373d27cefbd17d6c6791d8d1
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-09-15 06:06:02 +0300

    Azure registry for pushing containers.

--- deploy-all.sh
+++ deploy-all.sh
@@ -3,12 +3,12 @@ set -e
 
 # Builds and deploys all images for the Azure environments
 
-ORG=${ORG:-hsldevcom}
+ORG=${ORG:-hsl}
 
 # dev stage production
 TAG="latest"
 
-DOCKER_IMAGE="${ORG}/bultti-db:${TAG}"
+DOCKER_IMAGE="bulttiregistry.azurecr.io/${ORG}/bultti-db:${TAG}"
 
 docker build -t "$DOCKER_IMAGE" .
 docker push "$DOCKER_IMAGE"
