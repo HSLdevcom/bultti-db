9c0f2357e17d6835574ed13df53d0545bd061e1f
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-09-21 15:40:39 +0300

    Enable indexes concurrently separately from main departures query.

--- app/createDepartures.js
+++ app/createDepartures.js
@@ -5,6 +5,38 @@ import { departuresInsertQuery } from './queryFragments/departuresInsertQuery';
 
 const knex = getKnex();
 
+async function enableIndices(schemaName) {
+  console.log('[Status]   Enabling departure indices.');
+
+  await knex.raw(
+    `create index concurrently if not exists departure_stop_id_index on ${schemaName}.departure (stop_id)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_route_id_index on ${schemaName}.departure (route_id)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_day_type_index on ${schemaName}.departure (day_type)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_date_begin_index on ${schemaName}.departure (date_begin)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_route_id_direction_stop_id_idx on ${schemaName}.departure (route_id, direction, stop_id)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_stop_id_day_type on ${schemaName}.departure (stop_id, day_type)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_origin_time_index on ${schemaName}.departure (origin_hours, origin_minutes)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_departure_id_index on ${schemaName}.departure (departure_id)`
+  );
+  await knex.raw(
+    `create index concurrently if not exists departure_origin_index on ${schemaName}.departure (stop_id, route_id, direction, date_begin, date_end, departure_id, day_type)`
+  );
+}
+
 export async function createDepartures(schemaName, mainSync = false) {
   if (!mainSync && !startSync('departures')) {
     console.log('[Warning]  Syncing already in progress.');
@@ -16,6 +48,7 @@ export async function createDepartures(schemaName, mainSync = false) {
 
   try {
     await knex.raw(departuresInsertQuery, { schema: schemaName });
+    await enableIndices(schemaName);
   } catch (err) {
     console.log(`[Error]    Insert error on table departure`, err);
   }
